41e2091dca1e96324c6f1c8aae2e7bf1fb6fec8f
Jesse Huang <jesshuang5733@gmail.com>
2024-02-14 05:31:41 -0500

    help

--- src/Middleware/apiQueries.js
+++ src/Middleware/apiQueries.js
@@ -1,14 +1,5 @@
 /* eslint-disable no-undef */
 import { gql } from "@apollo/client";
-const cors = require("cors");
-const app = express();
-
-app.use(
-  cors({
-    origin: "https://hackthenorth-jesse.vercel.app",
-    credentials: true, // Enable credentials if you're sending cookies or other sensitive information
-  }),
-);
 
 export const GET_EVENTS = gql`
   query {
